5313d4fb6c687c09682900469ddd91ff888ac5d6
Silvio Bruciaferro <116919319+Bruci87@users.noreply.github.com>
2026-05-11 22:06:27 -0300

    Add files via upload

---
+++ Quest1IS.js
@@ -0,0 +1,12 @@
+class InsertionSort{
+    constructor(){
+        this.array = [1, 4, 8, 1, 2, 6, 7, 3];
+    }
+    sort(){
+        for(let i=0; i<=this.array.length; i++){
+            for(let j=0; j<=this.array.length -1; j++){
+
+            }
+        }
+    }
+}
